addc1952e7cc9ba7b2df408a41c8d5524b7267be
gudiya dwivedi <dwivedigudiyaa@gmail.com>
2025-11-20 16:51:48 +0530

    Added Day3 folder

---
+++ Day9/index.js
@@ -0,0 +1,108 @@
+ const express=require('express');
+const app=express();
+const main=require("./database");
+const User=require('./models/users')
+const validateUser=require('./utils/validateUser')
+const bcrypt=require('bcrypt');
+app.use(express.json());
+
+
+app.post("/register", async(req,res)=>{
+    try{
+
+        validateUser(req.body);
+     req.body.password= await bcrypt.hash(req.body.password,10);
+        
+       
+       await  User.create(req.body);
+       res.send("User register succesfully");
+
+    }
+    catch(err){
+        res.status(400).send("Error: "+err.message);
+
+    }
+})
+app.post("/login",async(req,res)=>{
+    try{
+        const people=await User.findById(req.body._id);
+        if(!(req.body.emailId===people.emailId))
+            throw new Error("invalid credentials");
+       const IsAllowed= bcrypt.compare(req.body.password,people.password);
+       if(!IsAllowed)
+        throw new Error("Invalid credentials");
+       res.send("Login Successfully");
+       
+
+
+
+    }
+    catch(err){
+
+    }
+})
+
+app.get("/info", async(req,res)=>{
+    try{
+    const ans=await User.find({});
+    res.send(ans);
+    }
+    catch(err){
+        res.status(400).send("Error:"+err.message);
+    }
+})
+app.get("/user/:id",async(req,res)=>{
+    try{
+        const result=await User.findById(req.params.id);
+        res.send(result);
+
+    }
+    catch(err){
+        res.send("Error:"+err.message());
+
+    }
+})
+
+app.delete("/user/:id",async(req,res)=>{
+    try{
+        await User.findByIdAndDelete(req.params.id);
+        res.send("deleted successfully");
+
+    }
+    catch(err){
+        res.send("Error: "+err.mesage());
+
+    }
+})
+app.patch("/user",async(req,res)=>{
+    try{
+        const {_id,...update}=req.body;
+        await User.findByIdAndUpdate(_id,update,{"runValidators":true});
+        res.send("updated successfully");
+    
+    }
+    catch(err){
+        res.send("Error:"+err.message);
+    }
+})
+
+
+
+
+
+
+
+
+
+main()
+.then(async ()=>{console.log("connected to db")
+app.listen(7500,()=>{
+    console.log("i am listening at port 7500");
+    
+})
+    // const result= await User.find({name:"Gudiya"});
+    // console.log(result);
+  
+})
+.catch((err)=>console.log(err));
+
